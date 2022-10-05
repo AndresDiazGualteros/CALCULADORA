2b3db4bad76718c5248badbca3fc8ceeba1af574
luis <andresgualteros2822@gmail.com>
2022-10-05 12:15:48 -0500

    version sin input

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const display = document.querySelector(".display");
 
-const valor = document.querySelectorAll(".valor");
+// const valor = document.querySelectorAll(".valor");
 
 const operador = document.querySelector(".operador");
 const suma = document.getElementById("suma");
@@ -9,49 +9,48 @@ const multi = document.getElementById("multi");
 const division = document.getElementById("division");
 const igual = document.getElementById("igual");
 const ac = document.getElementById("ac");
+const variable = document.getElementById("val1");
 
 suma.addEventListener("click",()=>{
     operador.innerText="+"
-    valor[1].value=valor[0].value
-    valor[0].value=""
+    variable.value=display.textContent
+    display.innerText=""
 })
 resta.addEventListener("click",()=>{
     operador.innerText="-"
-    valor[1].value=valor[0].value
-    valor[0].value=""
+    variable.value=display.textContent
+    display.innerText=""
 });
 multi.addEventListener("click",()=>{
     operador.innerText="*"
-    valor[1].value=valor[0].value
-    valor[0].value=""
+    variable.value=display.textContent
+    display.innerText=""
 });
 division.addEventListener("click",()=>{
     operador.innerText="/"
-    valor[1].value=valor[0].value
-    valor[0].value=""
+    variable.value=display.textContent
+    display.innerText=""
 });
 ac.addEventListener("click",()=>{
-    operador.innerText=""
+    operador.innerText="+"
     display.innerText=""
-    valor[0].value = ""
-    valor[1].value = ""
 });
 igual.addEventListener("click",()=>{
     if(operador.innerText==="+"){
-        display.innerText = parseInt(valor[1].value) + parseInt(valor[0].value)
+        display.innerText = parseInt(variable.value) + parseInt(display.textContent);
     }else if(operador.innerText==="-"){
-        display.innerText = parseInt(valor[1].value) - parseInt(valor[0].value)
+        display.innerText = parseInt(variable.value) - parseInt(display.textContent);
     }else if(operador.innerText==="*"){
-        display.innerText = parseInt(valor[1].value) * parseInt(valor[0].value)
+        display.innerText = parseInt(variable.value) * parseInt(display.textContent);
     }else if(operador.innerText==="/"){
-        display.innerText = parseInt(valor[1].value) / parseInt(valor[0].value)
+        display.innerText = parseInt(variable.value) / parseInt(display.textContent);
     }
 })
 
 for(let i=0; i<10; i++){
     document.getElementById("num"+i).addEventListener("click",presion)
 }
-
 function presion(e){
     display.innerText = display.innerText + e.target.value
 }
+
